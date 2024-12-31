be1fb10d9a89d62c5885f000bbd78475a45070ae
Java Student PC-14 <ajahid97@gmail.com>
2024-12-31 09:14:28 +0600

    SeventeenClass

---
+++ src/classSeventeen/TestOuter.java
@@ -0,0 +1,14 @@
+package classSeventeen;
+
+public class TestOuter {
+    public static void main(String[] args) {
+        // Create an instance of OuterClass
+        OuterClass outer = new OuterClass();
+
+        // Demonstrate InnerClass functionality
+        outer.showInnerClassDemo();
+
+        OuterClass.InnerClass innerClass = outer.new InnerClass();
+        innerClass.displayMessage();
+    }
+}
