d678a181a93be2307e8db79f240d8767e57fe492
Jahid Ahmed <ajahid97@gmail.com>
2025-01-13 01:16:05 +0600

    Practice

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
+        //System.out.println("Hello, World!");
 
 
         /*int age = 16;
@@ -35,7 +35,7 @@ public class Main {
         }*/
 
 
-        Scanner scanner = new Scanner(System.in);
+/*        Scanner scanner = new Scanner(System.in);
         int num = scanner.nextInt();
         for (int i = 0; i<=num; i++){
             if (i<=1){
@@ -46,7 +46,53 @@ public class Main {
             }else {
                 System.out.print((i-1)+(i-2)+" ");
             }
+        }*/
+
+      /*  int[] array = {1, 2, 5, 4, 3, 6, 9, 8};
+        int min = array[0];
+        int max = array[0];
+        for (int i: array){
+
+            if (min>i)
+                min = i;
+
+            if (max<i)
+                max =i;
         }
+
+        System.out.println("Minimum value is: "+min);
+        System.out.println("Maximum value is: "+max);*/
+
+        Scanner sc=new Scanner(System.in);
+
+while (true){
+    System.out.println("1. Grading System\n2. Exit");
+    System.out.print("Enter your choice: ");
+    int i = sc.nextInt();
+    if (i==1){
+        System.out.print("Enter your mark check grade: ");
+        int marks = sc.nextInt();
+        if (marks<0 || marks>100){
+            System.out.println("You enter invalid number. Please enter valid number.");
+        }else {
+            if (marks>80)
+                System.out.println("You got A+");
+            else if (marks>70)
+                System.out.println("You got A");
+            else if (marks>60)
+                System.out.println("You got A-");
+            else if (marks>50)
+                System.out.println("You got B");
+            else if (marks>40)
+                System.out.println("You got C");
+            else if (marks>32)
+                System.out.println("You got D");
+            else
+                System.out.println("Sorry you are fail. You got F");
+        }
+    }else
+        break;
+}
 
 
     }
