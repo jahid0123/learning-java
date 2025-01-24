da1695938face0576184ce24c21e735dc32a7b87
Jahid Ahmed <ajahid97@gmail.com>
2025-01-25 01:25:41 +0600

    Generic

---
+++ src/Generic/Container.java
@@ -0,0 +1,7 @@
+package Generic;
+
+public interface Container<T> {
+
+    void add (T item);
+    T get();
+}
---
+++ src/Generic/StringContainer.java
@@ -0,0 +1,19 @@
+package Generic;
+
+public class StringContainer<T> implements Container<T> {
+
+    private T item;
+
+
+
+    @Override
+    public void add(T item) {
+        this.item = item;
+
+    }
+
+    @Override
+    public T get() {
+        return item;
+    }
+}
